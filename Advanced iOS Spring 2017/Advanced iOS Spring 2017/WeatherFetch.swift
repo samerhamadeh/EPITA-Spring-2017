f972fc4fa036d39108d668d17afc6a50122ef5b4
emilehoyek <emilehoyek@MacBook-Pro-2.local>
2017-04-28 20:02:18 +0200

    WeatherFetch...

--- Advanced iOS Spring 2017/Advanced iOS Spring 2017/WeatherFetch.swift
+++ Advanced iOS Spring 2017/Advanced iOS Spring 2017/WeatherFetch.swift
@@ -10,15 +10,15 @@ import Foundation
 
 class WeatherFetch {
     
-    private let openWeatherMapBaseURL = "http://api.openweathermap.org/data/2.5/boogity"
-    private let openWeatherMapAPIKey = "Y0URK3YG03SH3R3"
+    private let openWeatherMapBaseURL = "http://api.openweathermap.org/data/2.5/weather"
+    private let openWeatherMapAPIKey = "3a67124719f5f172f6dc837653322565"
     
     func getWeather(city: String) {
         
         // This is a pretty simple networking task, so the shared session will do.
         let session = URLSession.shared
         
-        let weatherRequestURL = URL(string: "\(openWeatherMapBaseURL)?APPID=\(openWeatherMapAPIKey)&q=\(city)")!
+        let weatherRequestURL = URL(string: "\(openWeatherMapBaseURL)?q=\(city)&APPID=\(openWeatherMapAPIKey)")!
         
         // The data task retrieves the data.
         let dataTask = session.dataTask(with: weatherRequestURL) {
@@ -26,12 +26,15 @@ class WeatherFetch {
             if let error = error {
                 // Server has returned an error
                 print("Error:\n\(error)")
+            }
             
             else {
                 // Server has returned data
                 print("Raw data:\n\(data!)\n")
                 let dataString = String(data: data!, encoding: String.Encoding.utf8)
-                print("Human-readable data:\n\(data!)")
+                print("Human-readable data:\n\(data!)\n")
+                
+                print("Info:\n\(dataString!)")
             }
         }
         dataTask.resume()
