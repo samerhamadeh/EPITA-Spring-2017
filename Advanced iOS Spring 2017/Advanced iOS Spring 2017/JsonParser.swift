7c44a9957fb63a945fbce19dfffc6a72a7aadcd9
emilehoyek <emilehoyek@MacBook-Pro-2.local>
2017-05-25 00:54:41 +0200

    With TableView

---
+++ Advanced iOS Spring 2017/Advanced iOS Spring 2017/JsonParser.swift
@@ -0,0 +1,9 @@
+//
+//  JsonParser.swift
+//  Advanced iOS Spring 2017
+//
+//  Created by Emile Hoyek on 5/10/17.
+//  Copyright © 2017 ___AdvancediOS___. All rights reserved.
+//
+
+import Foundation
---
+++ Advanced iOS Spring 2017/Advanced iOS Spring 2017/Weather.swift
@@ -0,0 +1,9 @@
+//
+//  Weather.swift
+//  Advanced iOS Spring 2017
+//
+//  Created by Emile Hoyek on 5/17/17.
+//  Copyright © 2017 ___AdvancediOS___. All rights reserved.
+//
+
+import Foundation
